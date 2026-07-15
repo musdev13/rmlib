d3225bad491a818921e0696f1fa6b9e1f4365910
MusDev13 <sonics0977703996@gmail.com>
2026-07-16 00:16:16 +0300

    testing new functions from libs

--- src/core/version/install.rs
+++ src/core/version/install.rs
@@ -3,8 +3,19 @@ use std::path::PathBuf;
 use crate::core;
 
 pub async fn install(version_id: String, versions_path_option: Option<PathBuf>, libs_path_option: Option<PathBuf>){
-    let version_json_str = core::version::get_version_json(version_id).await;
-    println!("{}", version_json_str);
+    // let version_json_str = core::version::get_version_json(version_id).await;
+    // println!("{}", version_json_str);
+
+    let mut is_custom_versions_path = false;
+    let versions_path = musutils::fs::tilda_desir(
+        musutils::types::deoption(
+            versions_path_option, 
+            || PathBuf::from(core::config::get_versions_path()),
+            &mut is_custom_versions_path
+        )
+    );
+
+    println!("{}", versions_path.display());
 
     // let versions_path: PathBuf = directory.unwrap_or_else(|| {
     //     println!("using default path default path default... path..?");
